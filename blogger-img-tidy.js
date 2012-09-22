91f4671eed580609687733f30094b3a69b15e43d
bootleq <bootleq@gmail.com>
2012-09-22 17:27:52 +0800

    Initial revision.

---
+++ blogger-img-tidy.js
@@ -0,0 +1,20 @@
+var
+  $textarea = document.getElementById('postingHtmlBox'),
+  pattern = [
+    '<div class="separator" style="clear: both; text-align: center;">\n',
+    '<a href="(.+)"',
+    ' imageanchor="1" style=".*"><img border="0" height=".+" width=".+" ',
+    'src="(.+)" /></a></div>'
+  ].join(''),
+  re = new RegExp(pattern, 'g');
+
+if ($textarea) {
+  $textarea.value = $textarea.value.replace(re, function (str, p1, p2, p3) {
+    return [
+      '<a href="', p1, '">',
+      '<img src="', p2, '" alt="" /></a>'
+    ].join('');
+  });
+} else {
+  alert('錯誤，找不到 textarea#postingHtmlBox。');
+}
