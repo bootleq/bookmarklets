299818406950d615ae5837f56ad69d316801ddf4
bootleq <bootleq@gmail.com>
2013-04-07 12:22:31 +0800

    GA 過濾改了，setVar 應改為 setCustomVar。
    新方式
    https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingCustomVariables
    
    _setVar
    https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApiBasicConfiguration?hl=zh-TW#_gat.GA_Tracker_._setVar

--- google-analytics-self.filter.js
+++ google-analytics-self.filter.js
@@ -1,12 +1,13 @@
-if(typeof(_gat)=='undefined'){
-  alert("無法在這個網頁運作（我們找不到 _gat），抱歉。");
-  return;
-}
-var leqTracker = _gat._getTracker();
-if(leqTracker){
-  var v=prompt("請輸入要用於「篩選器模式」的字串：","test_value");
-  if(v!=null){
-    leqTracker._setVar(v);
-    alert("已設定 cookie: "+v);
+if (typeof(_gaq) === 'undefined' ) {
+  alert("無法在這個網頁運作（找不到 _gaq 物件），抱歉。");
+} else {
+  var v = prompt("請輸入要用於「篩選器模式」的字串：", "test_value");
+  if (v !== null) {
+    // See: https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingCustomVariables
+    _gaq.push(['_setCustomVar', 1, 'ex', v, 1]);
+    alert([
+      "已設定 cookie: " + v,
+      "SCRIPT: " + "_gaq.push(['_setCustomVar', 1, 'ex', " + v + ", 1]);"
+    ].join("\n"));
   }
 }
