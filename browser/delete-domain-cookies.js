5658b8cb6542c6989322aa14664183d8eb371af8
bootleq <bootleq@gmail.com>
2013-12-28 11:55:23 +0800

    改善「移除網域曲奇」功能

--- browser/delete-domain-cookies.js
+++ browser/delete-domain-cookies.js
@@ -8,31 +8,41 @@ var
   hostname = gBrowser.currentURI.host,
   cookies = manager.getCookiesFromHost(hostname),
   cookie,
-  killed = [];
+  dialogTitle = 'FireGestures script',
+  kills = [];
 
 while (cookies.hasMoreElements()) {
   cookie = cookies.getNext().QueryInterface(Ci.nsICookie2);
   if (cookie.rawHost === hostname || hostname.endsWith('.' + cookie.rawHost)) {
-    killed.push(cookie.name);
-    manager.remove(
-      cookie.host,
-      cookie.name,
-      cookie.path,
-      false
-    );
+    kills.push(cookie);
   }
 }
 
-if (killed.length) {
-  prompter.alert(
-    null,
-    'FireGestures script',
-    '已移除 ' + killed.length + " 個 cookie。\n\n主機：" + hostname + "\n名稱：" + killed.join("、")
-  );
+
+if (kills.length) {
+  if (prompter.confirm(
+        null,
+        dialogTitle,
+        [
+          hostname,
+          '移除 ' + kills.length + " 個曲奇？",
+          "\n  " + kills.map(function (cookie) { return cookie.name; }).join("\n  ")
+        ].join("\n")
+      )) {
+    kills.forEach(function (cookie) {
+      manager.remove(
+        cookie.host,
+        cookie.name,
+        cookie.path,
+        false
+      );
+    });
+    window.alert('已移除 ' + kills.length + " 個曲奇。");
+  }
 } else {
   prompter.alert(
     null,
-    'FireGestures script',
-    "沒有要移除的 cookie 了。\n\n主機：" + hostname
+    dialogTitle,
+    hostname + "\n沒有需移除的曲奇。"
   );
 }
